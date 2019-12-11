f83e0b04de8c0e78642dbfa1c89b2553b112f4f1
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-12-11 16:54:39 -0600

    added my headshot.

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Headshot from '../assets/images/my_headshot.jpg'
 
 function Bio() {
     return(
         <Row>
             <Col>
-                <p>This is a bio.</p>
+                <img src={Headshot} alt='Will Oakley' />
             </Col>
         </Row>
     );
