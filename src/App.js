54f2c2e8f73d5e8f9baa24440a2cd3cb488978f3
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-12-11 11:35:58 -0600

    Revert "Deleted some unneeded template files. Added some components. Added React Bootstrap. Going to switch to working on a branch now."
    This reverts commit b44ac79f54fd152f1ff651cd44fd8445c4994afa.
    
    rolling back commit. Seemed to break everything.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Bio from './components/Bio';
-import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Bio />
-      <Portfolio />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
