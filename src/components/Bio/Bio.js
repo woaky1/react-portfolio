f2e6310c9a87fa34c80c080d1574dca7b7a556e4
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-12-17 09:27:05 -0600

    Wrapped up general layout of bio box. Going to add linked in and git hub icons now.

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -12,12 +12,18 @@ function Bio() {
                 <Row>
                     <Col>
                         <div id="bioBox">
-                            <img src={Headshot} alt='Will Oakley' id="bioPic" />
-                            <p className="bioText">Hey there! My name is Will Oakley, a full stack web developer and graduate of Northwestern's Coding Bootcamp. I've worked in software development for twelve years as both a writer, a production specialist, and, now, a developer.</p>
-
-                            <p className="bioText">I'm interested in web development, javascript, and finding ways to apply technology to help others.</p>
-
-                            <p className="bioText">I'm also a <a href="https://ddb.ac/characters/2700895/iwpTsp" target="_blank" rel="noopener noreferrer">level 8 barbarian</a> in a <em>Dungeons &amp; Dragons</em> game with my friends.</p>
+                            <Row>
+                                <Col md={4}>
+                                    <img src={Headshot} alt='Will Oakley' id="bioPic" />
+                                </Col>
+                                <Col md={8}>
+                                    <p className="bioText">Hey there! My name is Will Oakley, a full stack web developer and graduate of Northwestern's Coding Bootcamp. I've worked in software development for twelve years as both a writer, a production specialist, and, now, a developer.</p>
+                                    {/* <br /> */}
+                                    <p className="bioText">I'm interested in web development, javascript, and finding ways to apply technology to help others.</p>
+                                    {/* <br /> */}
+                                    <p className="bioText">I'm also a <a href="https://ddb.ac/characters/2700895/iwpTsp" target="_blank" rel="noopener noreferrer">level 8 barbarian</a> in a <em>Dungeons &amp; Dragons</em> game with my friends.</p>
+                                </Col>
+                            </Row>
                         </div>
                     </Col>
                 </Row>
