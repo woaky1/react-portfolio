997ed329faa09481600e8ba527963c295d8650d1
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-12-18 16:20:33 -0600

    added my resume to the page.

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Headshot from './my_headshot.jpg'
-import LinkedIn from './linkedin-128.png'
-import GitHub from './github-128.png'
+import headshot from './my_headshot.jpg'
+import linkedIn from './linkedin-128.png'
+import gitHub from './github-128.png'
+import resume from './will_oakley_resume.pdf'
 import "./bioCSS.css"
 
 function Bio() {
@@ -25,7 +26,7 @@ function Bio() {
                             <Row>
                                 <Col lg={3} md={6}>
                                     <div >
-                                        <img src={Headshot} alt='Will Oakley' id="bioPic" className="img-fluid d-block m-auto"/>
+                                        <img src={headshot} alt='Will Oakley' id="bioPic" className="img-fluid d-block m-auto"/>
                                     </div>
                                 </Col>
                                 <Col lg={9} md={6}>
@@ -33,8 +34,9 @@ function Bio() {
                                         <p className="bioText">Hey there! My name is Will Oakley, a full stack web developer and graduate of Northwestern's Coding Bootcamp. I've worked in software development for twelve years as both a writer, a production specialist, and, now, a developer.</p>
                                         <p className="bioText">I'm interested in web development, javascript, and finding ways to apply technology to help others.</p>
                                         <p className="bioText">I'm also a <a href="https://ddb.ac/characters/2700895/iwpTsp" target="_blank" rel="noopener noreferrer">level 8 barbarian</a> in a <em>Dungeons &amp; Dragons</em> game with my friends.</p>
-                                        <a href="https://www.linkedin.com/in/william-oakley-49b7b618b/" target='_blank' rel='noopener noreferrer'><img src={LinkedIn} alt='Link to LinkedIn Page' className='linkIcon' id='linkedInIcon' /></a>
-                                        <a href="https://github.com/woaky1" target='_blank' rel='noopener noreferrer'><img src={GitHub} alt='Link to GitHub Page' className='linkIcon' id='gitHubIcon' /></a>
+                                        <p className="bioText">You can check out my resume <a href={resume}>here</a>.</p>
+                                        <a href="https://www.linkedin.com/in/william-oakley-49b7b618b/" target='_blank' rel='noopener noreferrer'><img src={linkedIn} alt='Link to LinkedIn Page' className='linkIcon' id='linkedInIcon' /></a>
+                                        <a href="https://github.com/woaky1" target='_blank' rel='noopener noreferrer'><img src={gitHub} alt='Link to GitHub Page' className='linkIcon' id='gitHubIcon' /></a>
                                     </div>
                                 </Col>
                             </Row>
