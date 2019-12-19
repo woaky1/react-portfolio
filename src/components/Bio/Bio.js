6b683047a2e490017099a50ffcb6629afe7a5f26
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-12-19 14:40:11 -0600

    made it so my resume opens up in a new tab and added an accent on the e in resume.

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -34,7 +34,7 @@ function Bio() {
                                         <p className="bioText">Hey there! My name is Will Oakley, a full stack web developer and graduate of Northwestern's Coding Bootcamp. I've worked in software development for twelve years as a writer, a production specialist, and, now, a developer.</p>
                                         <p className="bioText">I'm interested in web development, javascript, and finding ways to apply technology to help others.</p>
                                         <p className="bioText">I'm also a <a href="https://ddb.ac/characters/2700895/iwpTsp" target="_blank" rel="noopener noreferrer">level 8 barbarian</a> in a <em>Dungeons &amp; Dragons</em> game with my friends.</p>
-                                        <p className="bioText">You can check out my resume <a href={resume}>here</a>.</p>
+                                        <p className="bioText">You can check out my resum&#233; <a href={resume} target='_blank' rel='noopener noreferrer'>here</a>.</p>
                                         <a href="https://www.linkedin.com/in/william-oakley-49b7b618b/" target='_blank' rel='noopener noreferrer'><img src={linkedIn} alt='Link to LinkedIn Page' className='linkIcon' id='linkedInIcon' /></a>
                                         <a href="https://github.com/woaky1" target='_blank' rel='noopener noreferrer'><img src={gitHub} alt='Link to GitHub Page' className='linkIcon' id='gitHubIcon' /></a>
                                     </div>
