d1cde67a603ff575a68a484ffc4a20333102ade2
woaky1 <53833473+woaky1@users.noreply.github.com>
2020-01-07 11:28:12 -0600

    completed work on skills component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import './skillsCSS.css'
+import HTMLIcon from './icons/HTML5_Logo_128.png'
+import CSSIcon from './icons/HTML5_Styling_128.png'
+import JSIcon from './icons/240px-JavaScript-logo.png'
+import NodeIcon from './icons/320px-Node.js_logo.svg.png'
+import ReactIcon from './icons/logo192.png'
+import BootIcon from './icons/bootstrap-solid.svg'
 
 function Skills() {
     return (
@@ -8,22 +16,40 @@ function Skills() {
             <Col></Col>
             <Col md={10}>
                 <Row>
-                    <div>
-                        <h1>Skills</h1>
-                        <hr />
-                    </div>
-                </Row>
-                <Row>
-                    <Col>HTML5</Col>
-                    <Col>CSS3</Col>
-                    <Col>Javascript</Col>
-                    <Col>NodeJS</Col>
-                    <Col>React</Col>
-                    <Col>Bootstrap</Col>
+                    <Col>
+                        <div className="skillBox">
+                            <Row>
+                                <Col>
+                                    <h1>Skills</h1>
+                                    <hr />
+                                </Col>
+                            </Row>
+                            <Row style={{paddingBottom: "3rem"}}>
+                                <Col>
+                                    <Image className="icon" src={HTMLIcon} fluid />
+                                </Col>
+                                <Col>
+                                    <Image className="icon" src={CSSIcon} fluid />
+                                </Col>
+                                <Col>
+                                    <Image className="icon" src={JSIcon} fluid />
+                                </Col>
+                                <Col>
+                                    <Image className="icon" id="node" src={NodeIcon} fluid />
+                                </Col>
+                                <Col>
+                                    <Image className="icon" src={ReactIcon} fluid />
+                                </Col>
+                                <Col>
+                                    <Image className="icon" src={BootIcon} fluid />
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
                 </Row>
             </Col>
             <Col></Col>
-        </Row>
+        </Row >
     )
 }
 
