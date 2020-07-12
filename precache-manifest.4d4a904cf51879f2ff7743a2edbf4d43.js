c067cb74053eb6fe415370ca0e1717b4eda8c0cc
woaky1 <53833473+woaky1@users.noreply.github.com>
2020-07-12 11:27:49 -0500

    Updates

---
+++ precache-manifest.4d4a904cf51879f2ff7743a2edbf4d43.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbc6c87f96db76f8b82d5b98b89cbb6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3f00202a275bddf8118a",
+    "url": "/react-portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "656fcd275f64b8846af0",
+    "url": "/react-portfolio/static/css/main.5013baee.chunk.css"
+  },
+  {
+    "revision": "3f00202a275bddf8118a",
+    "url": "/react-portfolio/static/js/2.00e55ae8.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/react-portfolio/static/js/2.00e55ae8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "656fcd275f64b8846af0",
+    "url": "/react-portfolio/static/js/main.ea877202.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "0ad84c18239f965dcdcfec1618f5ff33",
+    "url": "/react-portfolio/static/media/320px-Node.js_logo.svg.0ad84c18.png"
+  },
+  {
+    "revision": "7da15861bccca5b8e0ab276258710b4a",
+    "url": "/react-portfolio/static/media/99.7da15861.jpg"
+  },
+  {
+    "revision": "7d8fca440aef03444fa3de62fd1fa225",
+    "url": "/react-portfolio/static/media/bamazon.7d8fca44.jpg"
+  },
+  {
+    "revision": "16fa1cbd467464183c18257777207ef4",
+    "url": "/react-portfolio/static/media/bootstrap-solid.16fa1cbd.svg"
+  },
+  {
+    "revision": "257c38d1f07558c35a0ce0750a44a95a",
+    "url": "/react-portfolio/static/media/burger.257c38d1.jpg"
+  },
+  {
+    "revision": "826122de80d57edb18385ae0d4e08dd9",
+    "url": "/react-portfolio/static/media/commute.826122de.jpg"
+  },
+  {
+    "revision": "f214006437bba23f7dbf853db2d4ff65",
+    "url": "/react-portfolio/static/media/crystals.f2140064.jpg"
+  },
+  {
+    "revision": "c902035caf01af242e410eef5ada2cb3",
+    "url": "/react-portfolio/static/media/friendfinder.c902035c.jpg"
+  },
+  {
+    "revision": "a3c9c19d1aae8d65caed6aa9e3fc2119",
+    "url": "/react-portfolio/static/media/liri.a3c9c19d.jpg"
+  },
+  {
+    "revision": "292556615176e7b8c47ff6cd8386fb83",
+    "url": "/react-portfolio/static/media/my_headshot.29255661.jpg"
+  },
+  {
+    "revision": "5b9d891caedbe504d75b51bc374bb3ee",
+    "url": "/react-portfolio/static/media/planit.5b9d891c.jpg"
+  },
+  {
+    "revision": "6888b788250133764f09b502088bb593",
+    "url": "/react-portfolio/static/media/pokeclick.6888b788.jpg"
+  },
+  {
+    "revision": "4affddb5601176c8f5554394d6f693e2",
+    "url": "/react-portfolio/static/media/psychic.4affddb5.jpg"
+  },
+  {
+    "revision": "f5e6f809f77640fd02dc21c4fa456d23",
+    "url": "/react-portfolio/static/media/school-info.f5e6f809.jpg"
+  },
+  {
+    "revision": "06ed9d97bd6df0553d22a78bb8c4438e",
+    "url": "/react-portfolio/static/media/traintracker.06ed9d97.jpg"
+  },
+  {
+    "revision": "16b8dfa17b783114ba053723b0b05c2a",
+    "url": "/react-portfolio/static/media/trivia.16b8dfa1.jpg"
+  },
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/react-portfolio/static/media/white-waves.a06a3248.png"
+  },
+  {
+    "revision": "6cae769f266877d7c8535d5ef7f46f73",
+    "url": "/react-portfolio/static/media/will_oakley_resume.6cae769f.pdf"
+  },
+  {
+    "revision": "aa9368798d7ee0367cc1ea74d32c827b",
+    "url": "/react-portfolio/static/media/wordguess_cli.aa936879.jpg"
+  }
+]);
